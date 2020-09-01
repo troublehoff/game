0b7058263e4f35fefae146a8d8c8b1c90b7d4b42
Steve Lewis <stelewi@googlemail.com>
2020-09-01 18:09:50 +0100

    initial commit - framework skeleton, basic controller and app.js

---
+++ assets/js/app.js
@@ -0,0 +1,74 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/app.css';
+import * as THREE from 'three';
+import * as Honeycomb from 'honeycomb-grid';
+import Hexmap from './hexmap';
+import $ from 'jquery';
+
+
+// Create an empty scene
+var scene = new THREE.Scene();
+
+// Create a basic perspective camera
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 4;
+
+// Create a renderer with Antialiasing
+var renderer = new THREE.WebGLRenderer({antialias:true});
+var viewport = document.getElementById('viewport');
+
+// Configure renderer clear color
+renderer.setClearColor("#000000");
+
+// Configure renderer size
+renderer.setSize( viewport.clientWidth, viewport.clientHeight );
+
+// Append Renderer to DOM
+viewport.appendChild( renderer.domElement );
+
+// ------------------------------------------------
+// FUN STARTS HERE
+// ------------------------------------------------
+
+// Create a Cube Mesh with basic material
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshLambertMaterial( { color: "#433F81" } );
+var cube = new THREE.Mesh( geometry, material );
+
+// Add cube to Scene
+scene.add( cube );
+
+var light = new THREE.PointLight(0xff5050);
+light.position.set(0, 5, 10);
+light.intensity = 2;
+scene.add(light);
+
+var ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const hexmap = new Hexmap(10, 10);
+hexmap.buildHexGeometry();
+// hexmap.buildMeshes(scene);
+
+
+// Render Loop
+var render = function () {
+    requestAnimationFrame( render );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+};
+
+render();
---
+++ assets/js/hexmap.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+import * as Honeycomb from 'honeycomb-grid';
+
+export default class Hexmap {
+
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.geometry = null;
+        this.Hex = Honeycomb.extendHex({ size: 5 })
+        const Grid = Honeycomb.defineGrid(this.Hex)
+        this.grid = Grid.rectangle({ width:  this.width, height: this.height });
+        this.hexMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    }
+
+    buildHexGeometry() {
+
+        var shape = new THREE.Shape();
+
+        const [a, b, ...rest] = [10, 20, 30, 40, 50];
+
+        // separate the first from the other corners
+        const [firstCorner, ...otherCorners] = this.Hex().corners();
+
+        shape.moveTo( firstCorner.x, firstCorner.y );
+        otherCorners.forEach(corner => shape.lineTo(corner.x, corner.y));
+        shape.lineTo( firstCorner.x, firstCorner.y );
+
+        var extrudeSettings = {
+            steps: 2,
+            depth: 16,
+            bevelEnabled: true,
+            bevelThickness: 1,
+            bevelSize: 1,
+            bevelOffset: 0,
+            bevelSegments: 1
+        };
+
+        this.geometry = new THREE.ExtrudeBufferGeometry( shape, extrudeSettings );
+    }
+
+    /**
+     * @param scene
+     */
+    buildMeshes(scene) {
+
+        this.grid.forEach(hex => {
+            const point = hex.toPoint();
+
+            var mesh = new THREE.Mesh( this.geometry, this.hexMaterial ) ;
+
+            mesh.setPosition(point.x, 0, point.y);
+            scene.add( mesh );
+        });
+    }
+
+}
